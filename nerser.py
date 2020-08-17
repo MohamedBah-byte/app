13779bf738f1e416f4fc64f6bea970084e7033c5
MohamedBah-byte <61213458+MohamedBah-byte@users.noreply.github.com>
2020-08-17 20:06:07 +0100

    Update nerser.py

--- nerser.py
+++ nerser.py
@@ -2,15 +2,18 @@ from flask import Flask
 from flask import request
 from flask_restful import Api, Resource, reqparse
 import stanza
-app = Flask(__name__)
+from json2html import *
 
+app = Flask(__name__)
 
 @app.route('/ner', methods=['GET'])
 def ner():
       nlp=stanza.Pipeline('fr')
       text=request.args.get('text', default = '', type = str)
       doc = nlp(text)
-      return doc.entities()
+      result=json2html.convert(json = doc.entities(), table_attributes="id=\"info-table\" class=\"table table-bordered table-hover\"")
+      
+      return result
    
 if __name__ == '__main__':
     app.run(port=5005, debug=True)
