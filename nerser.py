5cc9d25a83044e3e626aa5b4b11b8b0dd9f8240a
MohamedBah-byte <61213458+MohamedBah-byte@users.noreply.github.com>
2020-08-17 17:51:00 +0100

    Update nerser.py

--- nerser.py
+++ nerser.py
@@ -3,14 +3,13 @@ from flask_restful import Api, Resource, reqparse
 import stanza
 stanfordnlp.download('fr')
 app = Flask(__name__)
-api = Api(app)
-nlp=stanza.Pipeline('fr')
 
-class NEREntinty(Resource):
-    def get(self, text):
-        doc = nlp(text)
-        return doc.entities()
 
-api.add_resource(NEREntinty)
+@app.route("/")
+def get(self, text):
+      nlp=stanza.Pipeline('fr')
+      doc = nlp(text)
+      return doc.entities()
+
 if __name__ == '__main__':
     app.run(port='5005')
