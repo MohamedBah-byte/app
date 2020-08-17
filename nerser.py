89d3d8430e70459ccc80cc0629a2e73a9d6ec178
MohamedBah-byte <61213458+MohamedBah-byte@users.noreply.github.com>
2020-08-17 15:07:48 +0100

    Create nerser.py

---
+++ nerser.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import stanza
+
+app = Flask(__name__)
+api = Api(app)
+nlp=stanza.Pipeline('fr')
+
+class NEREntinty(Resource):
+    def get(self, text):
+        doc = nlp(text)
+        return doc.entities()
+
+api.add_resource(NEREntinty)
+if __name__ == '__main__':
+    app.run(port='5005')
