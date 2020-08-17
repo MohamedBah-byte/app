5e69d032b9a609398615ce10793be61b53aaa13d
MohamedBah-byte <61213458+MohamedBah-byte@users.noreply.github.com>
2020-08-17 21:07:09 +0100

    Update nerser.py

--- nerser.py
+++ nerser.py
@@ -11,6 +11,7 @@ def ner():
       nlp=stanza.Pipeline('fr')
       text=request.args.get('text', default = '', type = str)
       doc = nlp(text)
-      return json.dumps(doc.entities)   
+      return print(*[f'entity: {ent.text}\ttype: {ent.type}' for sent in doc.sentences for ent in sent.ents], sep='\n')
+
 if __name__ == '__main__':
     app.run(port=5005, debug=True)
