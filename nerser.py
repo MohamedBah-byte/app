f844d71fd2d1f575f010b7e62ae82187bd0feed3
MohamedBah-byte <61213458+MohamedBah-byte@users.noreply.github.com>
2020-08-18 12:31:16 +0100

    Update nerser.py

--- nerser.py
+++ nerser.py
@@ -12,11 +12,12 @@ app = Flask(__name__)
 
 @app.route('/ner', methods=['GET', 'POST'])
 def ner():
-      stanza.download('fr')
+      
       nlp=stanza.Pipeline('fr')
       text=request.args.get('text')
       doc = nlp(text)
       return str(doc.entities).strip('[]')
 
 if __name__ == '__main__':
+      stanza.download('fr')
     app.run(port=5005, debug=True)
